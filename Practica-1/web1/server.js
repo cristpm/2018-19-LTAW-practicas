a3a17a266911168384b569f906183c26280ea257
cristpm <crisdario21@gmail.com>
2019-01-30 13:06:33 +0100

    servidor de documentos html

--- Practica-1/web1/server.js
+++ Practica-1/web1/server.js
@@ -1,12 +1,17 @@
 var http = require('http');
-
-console.log("Arrancando servidor...")
-
+var fs = require('fs');// manejar archivos
+var url = require('url');// parsear url
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-  res.write(req.url);
-  res.end();
-  console.log("Peticion atendida")
+  var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function(err, data) {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found");
+      }
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
 }).listen(8080);
