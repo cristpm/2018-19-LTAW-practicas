c40817c603d9ed3cb125e816cab3f3b7cefe0f89
cristpm <crisdario21@gmail.com>
2019-04-28 20:14:51 +0200

    añadido interfaz grafica chat electron

---
+++ Practica-5/P5-Chat-Electron/chat-server.js
@@ -0,0 +1,66 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var num_usuarios = 0;
+
+//--Servir la pagina principal
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+  console.log("Página principal: /")
+});
+
+//-- Servir el cliente javascript
+app.get('/chat-client.js', function(req, res){
+  res.sendFile(__dirname + '/chat-client.js');
+  console.log("Fichero js solicitado")
+});
+
+//-- Servir el cliente css
+app.get('/styles.css', function(req, res){
+  res.sendFile(__dirname + '/styles.css');
+  console.log('Fichero css solicitado');
+})
+
+//-- Lanzar el servidor
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+
+//-- Evento: Nueva conexion recibida
+//-- Un nuevo cliente se ha conectado!
+io.on('connection', function(socket){
+  console.log('--> Usuario conectado!');
+  socket.emit("new_message", 'Bienvenido al chat');
+  socket.broadcast.emit('new_message', 'Un nuevo usuario se ha conectado');
+  num_usuarios += 1;
+
+  //-- Detectar si el usuario se ha desconectado
+  socket.on('disconnect', function(){
+    console.log('--> Usuario Desconectado');
+  });
+
+  //-- Detectar si se ha recibido un mensaje del cliente
+  socket.on('new_message', msg => {
+    var res = msg.split("/");
+    //-- Notificarlo en la consola del servidor
+    if ( res[res.length-1] == 'help') {
+      socket.emit("new_message",
+      " COMANDOS:<br>/help:Mostrará una lista con todos los comandos soportados <br>" +
+      "/list: Devolverá el número de usuarios conectados <br>" +
+      "/hello: El servidor nos devolverá el saludo <br>" +
+      "/data: Nos devolverá la fecha");
+    } else if ( res[res.length-1] == 'list') {
+      socket.emit("new_message", "numero de usuarios: " + num_usuarios);
+    } else if ( res[res.length-1] == 'hello') {
+      socket.emit("new_message", "hello usuario");
+    } else if ( res[res.length-1] == 'date') {
+      socket.emit("new_message", "" + new Date());
+    } else {
+      //-- Emitir un mensaje a todos los clientes conectados
+      io.emit('new_message', msg);
+      console.log(msg)
+    }
+  })
+
+});
---
+++ Practica-5/P5-Chat-Electron/client-app.js
@@ -0,0 +1,5 @@
+function main()
+{
+  console.log("Estoy en app.js...")
+  }
+}
